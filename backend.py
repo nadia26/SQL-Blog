31077cc4cd795a0d64d9a8dfd7961f09dc220aa1
Max Zhou <ball908765@gmail.com>
2014-10-22 13:21:23 -0400

    moved off the csv, finished API for adding/getting stuff

--- backend.py
+++ backend.py
@@ -1,14 +1,15 @@
 import sqlite3
 import csv
+import time
 
 com= sqlite3.connect('test.db')
 c=com.cursor()
-
+'''
 BASE="INSERT INTO posts VALUES('%(title)s','%(body)s')"
 for l in csv.DictReader(open('posts.csv')):
-    print l
+    #print l
     q = BASE%l
-    print q
+    #print q
     c.execute(q)
 
 BASE="""
@@ -17,25 +18,69 @@ INSERT INTO comments
 """
 for l in csv.DictReader(open('comments.csv')):
     q = BASE%l
-    print q
+    #print q
     c.execute(q)
 
 com.commit()
 
-def getPosts(title):
-    q = "SELECT * FROM posts WHERE posts.title='" + title+"'"
-    posts = c.execute(q)
-    com.commit()
-    return posts
+'''
+
 
 def addPost(title, body):
-    q = "INSERT INTO posts VALUES('" + title + "','" + body + "')"
+    q = "INSERT INTO posts VALUES('" + title + "','" + body + "','" + str(time.time()) + "')"
     print q
     c.execute(q)
     com.commit()
 
-addPost('testing...', 'need to sanitize commas at least')
+def addComment(title, comment):
+    q = "INSERT INTO comments VALUES('" + title + "','" + comment + "','" + str(time.time()) + "')"
+    print q
+    c.execute(q)
+    com.commit()
+
+
+def getPosts(title):
+    '''returns a one-element array containing a tuple (body, timestamp)
+    '''
+    q = "SELECT * FROM posts WHERE posts.title='" + title+"'"
+    posts = c.execute(q)
+    com.commit()
+    temp = [x[1:3] for x in posts]
+    return temp
+
+
+def getComments(title): #need to add sanitization on add and get: sanitize at the beginning of add and in temp
+    #but tuples are immutable, so put it in the list comprehension perhaps? not really sure, may need to do more stuff. D:
+    '''returns an array of tuples (comment, timestamp)
+    '''
+    q = "SELECT * FROM comments WHERE comments.title='" + title + "'"
+    comments = c.execute(q)
+    com.commit()
+    temp = [x[1:3] for x in comments]
+    return timeSort(temp)
+#    return comments
+
+def timeSort(blurbs): #blurps is an array of tuples, with the first element in the tuple being the text and the second being the time
+    blurbs = sorted(blurbs, key = lambda tstamp: tstamp[1])
+    
+    return blurbs
+
+
+addPost('pieeeeeeeeeeeeeeeeeeeeeeeee', 'I love pie!')
+time.sleep(1)
+addPost('testing...', 'Is anybody here?')
+time.sleep(1)
+addComment('pieeeeeeeeeeeeeeeeeeeeeeeee', 'need to sanitize commas at least')
+time.sleep(1)
+addComment('pieeeeeeeeeeeeeeeeeeeeeeeee', 'cake is better')
+time.sleep(1)
+addComment('testing...', 'testing123')
+time.sleep(1)
 
 a = getPosts('testing...')
-for x in a:
-    print x
+b = getComments('pieeeeeeeeeeeeeeeeeeeeeeeee')
+#print a
+print a
+print b
+
+
--- init.py
+++ init.py
@@ -3,9 +3,9 @@ import sqlite3
 
 com= sqlite3.connect('test.db')
 c=com.cursor()
-q = "CREATE TABLE posts(title TEXT UNIQUE, body TEXT)"
+q = "CREATE TABLE posts(title TEXT UNIQUE, body TEXT, time FLOAT)"
 c.execute(q)
-q = "CREATE TABLE comments(title TEXT, comment TEXT)"
+q = "CREATE TABLE comments(title TEXT, comment TEXT, time FLOAT)"
 c.execute(q)
 com.commit()
 
